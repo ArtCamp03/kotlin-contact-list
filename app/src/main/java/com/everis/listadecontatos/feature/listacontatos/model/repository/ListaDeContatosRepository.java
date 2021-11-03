ae6628a567feae7a1f77f1ca736342cafc8d7edc
Artur R Campos <arturrc10@outlook.com>
2021-11-03 09:00:02 -0300

    att

--- app/src/main/java/com/everis/listadecontatos/feature/listacontatos/model/repository/ListaDeContatosRepository.java
+++ app/src/main/java/com/everis/listadecontatos/feature/listacontatos/model/repository/ListaDeContatosRepository.java
@@ -1,8 +1,15 @@
 package com.everis.listadecontatos.feature.listacontatos.model.repository;
 
+import com.everis.listadecontatos.feature.listacontatos.model.ContatosVO;
+import kotlin.Unit;
+
+import java.util.List;
+
 public class ListaDeContatosRepository {
     fun requestListaDeContatos(
             busca: String
+            onSucesso: ((List<ContatosVO> -> Unit)),
+            onError: ((Fxception -> Unit)),
     ){
 
     }
